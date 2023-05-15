4c70a76dfbd2ebfd1f33dc1f5eca7625a4c536fe
Pablo <Pablo@DESKTOP-FVUDCBN>
2023-05-15 09:08:30 +0200

    Comentario examen

---
+++ EXAMEN_16M_DAM_PABLODIAZGARCIA/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Pablo
+ *
+ */
+module EXAMEN_16M_DAM_PABLODIAZGARCIA {
+}
---
+++ EXAMEN_16M_DAM_PABLODIAZGARCIA/src/pkt/Examen.java
@@ -0,0 +1,23 @@
+package pkt;
+
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args) {
+		Scanner ent = new Scanner (System.in);
+		int num=0;
+		
+		System.out.println("Hola Marta soy Pablo, Introduce un número para saber si es par o no");
+		num=ent.nextInt();
+		
+		if(num/2==0) {
+			System.out.println("Tu número " + num + " es par" );
+		}else {
+			System.out.println("Tu número " + num + " es impar" );
+		}
+		
+		
+	}
+
+}
